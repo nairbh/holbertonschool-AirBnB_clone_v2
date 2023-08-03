f1c6511b78220c7c2dd2027a16949254a496d801
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-03 14:21:52 +0200

    console

--- console.py
+++ console.py
@@ -161,8 +161,6 @@ class HBNBCommand(cmd.Cmd):
         new_instance = HBNBCommand.classes[class_name](**params)
         new_instance.save()
 
-        print(new_instance.id)
-
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
