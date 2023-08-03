0ba6508a97c9a0c8965d62bf8f51cfc6867113b2
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-08-03 14:01:27 +0200

    console

--- console.py
+++ console.py
@@ -10,6 +10,8 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+import shlex
+from datetime import datetime
 
 
 class HBNBCommand(cmd.Cmd):
@@ -73,7 +75,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,13 +120,48 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        arg_list = shlex.split(args)
+        class_name = arg_list[0]
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        arg_list = arg_list[1:]
+
+        params = {}
+
+        for param in arg_list:
+            if '=' not in param:
+                continue
+
+            key, value = param.split('=', 1)
+
+            key = key.replace('_', ' ')
+
+            if value.startswith('"') and value.endswith('"'):
+                value = value[1:-1]
+
+                value = value.replace('\\"', '"')
+
+            if key in HBNBCommand.types:
+                try:
+                    value = HBNBCommand.types[key](value)
+
+                except ValueError:
+                    print(f"Invalid value for parameter '{key}': {value}")
+                    return
+       
+            if key == 'updated_at':
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+        
+            params[key] = value
+
+        new_instance = HBNBCommand.classes[class_name](**params)
+        new_instance.save()
+
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +309,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +317,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] =='\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +356,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
