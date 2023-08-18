00ef92fe97d1b3d72784237e4ecdd447c2246ea8
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-18 15:47:50 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,12 +12,13 @@ class FileStorage:
         """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
-        else:
-            new_dict = {}
-            for key in FileStorage.__objects:
-                if FileStorage.__objects[key].__class__ == cls:
-                    new_dict[key] = FileStorage.__objects[key]
-            return new_dict
+        new_dict = {}
+        name_cls = cls.__name__
+        key = FileStorage.__objects.keys()
+        for i in key:
+            if name_cls == i.split(".")[0]:
+                new_dict[i] = FileStorage.__objects[i]
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -41,7 +42,6 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
         classes = {
             'BaseModel': BaseModel, 'User': User, 'Place': Place,
             'State': State, 'City': City, 'Amenity': Amenity,
@@ -58,9 +58,9 @@ class FileStorage:
 
     def delete(self, obj=None):
         """function that deletes an object"""
-        if obj:
-            del (FileStorage.__objects["{}.{}".format
-                                       (obj.__class__.__name__, obj.id)])
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            del (FileStorage.__objects[key])
 
     def close(self):
         """ Function that call the reload method """
