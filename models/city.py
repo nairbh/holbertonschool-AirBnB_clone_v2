a66751d5b3b1ec42de876e70af6d7901f6f1e41a
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-18 15:04:58 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.city import City
+from models import storage
+from os import getenv
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"),
-                      nullable=False)
-
-    places = relationship("Place", backref="cities", cascade="all, delete",
-                          passive_deletes=True)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete",
+                              passive_deletes=True)
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances"""
+            new_list = []
+            all_cities = storage.all(City)
+            for element in all_cities.values():
+                if self.id == element.state_id:
+                    new_list.append(element)
+            return new_list
