ce2681c73f13e7b89973f754da7320cc08f7a2c7
Houcine <113390396+nairbh@users.noreply.github.com>
2023-08-18 15:45:49 +0200

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import models
+from models.place import Place
 from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id', ondelete="CASCADE"),
-                      nullable=False)
+    state_id = Column(String(128), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade='delete')
 
-    places = relationship("Place", backref="cities", cascade="all, delete",
-                          passive_deletes=True)
+    def __init__(self, *args, **kwargs):
+        """Initialization inherited"""
+        super().__init__(*args, **kwargs)
