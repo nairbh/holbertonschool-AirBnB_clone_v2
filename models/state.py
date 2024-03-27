78470a75d43f7c81e4748162d5fa8c048165d185
savvyh <7277@holbertonstudents.com>
2024-03-27 13:52:17 +0000

    Come back to the previous code to find the probleme

--- models/state.py
+++ models/state.py
@@ -6,6 +6,4 @@ from sqlalchemy import Column, String
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    
-    name = Column(String(128), nullable=False)
+    name = ""
