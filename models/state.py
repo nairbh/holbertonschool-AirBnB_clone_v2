c54e149814340c4ce213ec78098a0a59cec7a754
savvyh <7277@holbertonstudents.com>
2024-03-28 09:03:24 +0000

    Modify the name of the argument in 'relationship'

--- models/state.py
+++ models/state.py
@@ -10,5 +10,5 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", base_populates="state",
+    cities = relationship("City", back_populates="state",
                           cascade="all, delete, delete-orphan")
